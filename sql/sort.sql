e4258dc1a0cccf4447c0de48b6f0694abd3e2975
ao9 <ao9@iu.edu>
2025-06-23 16:25:34 -0500

    removed folder

--- sql/sort.sql
+++
@@ -1,4 +0,0 @@
-SELECT subject, COUNT(*) AS transaction_count
-FROM transactions
-GROUP BY subject
-ORDER BY transaction_count DESC;
--- sql/sort2.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  strftime('%H', date) AS hour_of_day,
-  COUNT(*) AS transaction_count,
-  AVG(amount) AS avg_amount
-FROM transactions
-GROUP BY hour_of_day
-ORDER BY hour_of_day;
